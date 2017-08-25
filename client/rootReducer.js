73d84b5212a9d80a0983ba7c6e2e0dc7eb4d1f1c
Thomas Run <thomasrun0411@gmail.com>
2017-08-25 09:14:21 -0400

    remove origin structure

--- client/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import flashMessages from './reducers/flashMessages';
-import auth from './reducers/auth';
-
-export default combineReducers({
-  flashMessages,
-  auth
-});
