001cd8203a142b8dac0505783269c9d9afd819a6
kuissoft <fivewisdomzero@gmail.com>
2017-09-05 21:18:44 -0400

    replace json with api

--- src/actions/marketsActions.js
+++ src/actions/marketsActions.js
@@ -1,4 +1,5 @@
 import { SET_MARKETS } from './types';
+import axios from 'axios';
 
 export function setMarkets(markets) {
   return {
@@ -9,6 +10,14 @@ export function setMarkets(markets) {
 
 export function fetchMarkets() {
   return dispatch => {
+    return axios.get('https://mkt-stag.herokuapp.com/api/markets').then(res => {
+      debugger;
+      if (res.status !== 200) {
+        console.log(`There was a problem: ${res.status}`);
+        return;
+      }
+      console.log(res);
+    });
     /*fetch('https://mkt-stag.herokuapp.com/api/markets', {
       mode: 'no-cors',
       headers: {
@@ -18,21 +27,27 @@ export function fetchMarkets() {
       }
     })
     .then(res => {
+      if (res.status !== 200) {
+        console.log(`There was a problem: ${res.status}`);
+        return;
+      }
+
       console.log(res);
       return res.json();
     }).then(data => {
       console.log(data);
       dispatch(setMarkets(data.markets))
     });*/
-    fetch("src/data/data.json")
-    .then(response => {
-      if (response.status !== 200) {
-        console.log(`There was a problem: ${response.status}`)
-        return;
-      }
-      response.json().then(data => {
-        dispatch(setMarkets(data.markets));
-      });
-    });
+
+    // fetch("src/data/data.json")
+    // .then(response => {
+    //   if (response.status !== 200) {
+    //     console.log(`There was a problem: ${response.status}`)
+    //     return;
+    //   }
+    //   response.json().then(data => {
+    //     dispatch(setMarkets(data.markets));
+    //   });
+    // });
   }
 }
