c68f06388ee4d5147d5340735b221397c9f21b8e
kuissoftech <fivewisdomzero@gmail.com>
2017-09-09 22:46:03 -0400

    modify CompanyCard

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/stylesheets/components/CompanyCard.scss';
 
 class CompanyCard extends React.Component {
   constructor(props) {
@@ -14,16 +15,31 @@ class CompanyCard extends React.Component {
             <img src={company.imageSrc} alt="Company Logo" />
           </div>
           <div className="content">
-            <a className="header">Header</a>
+            <a className="header">{company.name}</a>
+            <p>{company.website}</p>
             <div className="meta">
-              <span>Description</span>
+              <span>{company.description}</span>
             </div>
             <div className="description">
               <p></p>
             </div>
             <div className="extra">
-              <div className="ui label">IMAX</div>
-              <div className="ui label"><i className="globe icon"></i> Additional Languages</div>
+              <div className="ui label">
+                <i className="fa fa-flag" aria-hidden="true"></i>
+                <p>{company.geo.city}<br />{company.geo.stateCode},{company.geo.countryCode}</p>
+              </div>
+              <div className="ui label">
+                <i className="fa fa-star" aria-hidden="true"></i>
+                <p>{company.foundedYear}<br />founded</p>
+              </div>
+              <div className="ui label">
+                <i className="fa fa-users" aria-hidden="true"></i>
+                <p>{company.metrics.employees}<br />employees</p>
+              </div>
+              <div className="ui label">
+                <img src={company.addedToMarketBy.imageSrc} alt="Avatar" width="20" />
+                <p>Added by<br />{company.addedToMarketBy.name}</p>
+              </div>
             </div>
           </div>
         </div>
